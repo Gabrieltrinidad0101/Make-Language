902a322b2609ad1a72dfda8c77d7cca96a294797
gabriel <gabrielqwes123@gmail.com>
2024-05-06 15:05:17 -0400

    feat: ValidateTypes

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 	"makeLanguages/src/features/booleans"
 	"makeLanguages/src/interprete/interpreteStructs"
 	"makeLanguages/src/parser/parserStructs"
+	"makeLanguages/src/utils"
 )
 
 func lessOrGreaterOne(value1 interpreteStructs.IBaseElement, value2 interpreteStructs.IBaseElement) interface{} {
+	params := &[]interpreteStructs.IBaseElement{
+		value1,
+		value2,
+	}
+	utils.ValidateTypes(params, "Number", "Number")
 	number1 := value1.GetValue().(float64)
 	number2 := value2.GetValue().(float64)
 	boolean := number1+1 == number2 || number1-1 == number2
--- src/utils/getType.go
+++ src/utils/getType.go
@@ -1,6 +1,12 @@
 package utils
 
-import "reflect"
+import (
+	"fmt"
+	"makeLanguages/src/constants"
+	"makeLanguages/src/customErrors"
+	"makeLanguages/src/interprete/interpreteStructs"
+	"reflect"
+)
 
 func GetType(node interface{}) string {
 	method := reflect.TypeOf(node)
@@ -11,5 +17,14 @@ func GetType(node interface{}) string {
 		return method.Elem().Name()
 	} else {
 		return method.Name()
+	}
+}
+
+func ValidateTypes(params *[]interpreteStructs.IBaseElement, types ...string) {
+	for i, type_ := range types {
+		paramType := GetType((*params)[i])
+		if paramType != type_ {
+			customErrors.RunTimeError((*params)[i], fmt.Sprintf("Invalid params got %s expect %s", paramType, type_), constants.STOP_EXECUTION)
+		}
 	}
 }
